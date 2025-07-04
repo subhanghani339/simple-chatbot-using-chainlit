2b67d40d99a98337a8dc88a644061ab875b3d295
Muhammad Subhan <subhanghani339@gmail.com>
2025-07-04 23:48:26 +0500

    simply initialzied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def main():
-    print("Hello from simple-chatbot-using-chainlit!")
+import chainlit as cl
 
-
-if __name__ == "__main__":
-    main()
+@cl.on_message
+async def on_message(message: cl.Message):
+    response = f"Hello, you just send: {message.content}!"
+    await cl.Message(response, "subhan").send()
